75258b822d4291dfaaae99355aa5fdd039319450
Ricardo <ricorpwd@yahoo.co.za>
2022-06-14 11:01:18 +0200

    Update LeadRepository.java

--- src/main/java/org/greatgamesonly/shared/opensource/sql/framework/lightweightsql/example/LeadRepository.java
+++ src/main/java/org/greatgamesonly/shared/opensource/sql/framework/lightweightsql/example/LeadRepository.java
@@ -31,7 +31,7 @@ public class LeadRepository extends BaseRepository<Lead> {
         return leads != null && leads.size() > 0 ? leads.get(0) : null;
     }
 
-    public List<Lead> getAllAfterCreateDateByStatusRawQuery(java.sql.Timestamp afterThisDate) throws RepositoryException, SQLException {
+    public List<Lead> getAllAfterCreateDateRawQuery(java.sql.Timestamp afterThisDate) throws RepositoryException, SQLException {
         List<Lead> result = new ArrayList<>();
         ResultSet resultSet = super.executeGetQueryRaw("SELECT * from leads WHERE create_date > '"+afterThisDate.toString()+
                 "' ORDER BY create_date DESC;");
