dfe674b8cd34f4bffc6dc344f6333544bcdd7f28
Ricardo Minnaar <ricorpwd@yahoo.co.za>
2023-04-06 13:46:47 +0200

    replaced ResourceUtils.getProperties() with ResourceUtils.getProperty()

--- src/main/java/org/greatgamesonly/shared/opensource/sql/framework/lightweightsql/database/DbConnectionDetailsManager.java
+++ src/main/java/org/greatgamesonly/shared/opensource/sql/framework/lightweightsql/database/DbConnectionDetailsManager.java
@@ -27,54 +27,47 @@ public class DbConnectionDetailsManager {
     }
 
     protected static String getDatabaseUrl() {
-        String result = getProperties().getProperty("datasource.url");
+        String result = ResourceUtils.getProperty("datasource.url");
         if(result == null || result.isBlank()) {
-            result = getProperties().getProperty("quarkus.datasource.url");
+            result = ResourceUtils.getProperty("quarkus.datasource.url");
         }
         if(result == null || result.isBlank()) {
-            result = getProperties().getProperty("DATABASE_URL");
+            result = ResourceUtils.getProperty("DATABASE_URL");
         }
         return result;
     }
 
     protected static String getDatabaseUsername() {
-        String result = getProperties().getProperty("datasource.username");
+        String result = ResourceUtils.getProperty("datasource.username");
         if(result == null || result.isBlank()) {
-            result = getProperties().getProperty("quarkus.datasource.username");
+            result = ResourceUtils.getProperty("quarkus.datasource.username");
         }
         if(result == null || result.isBlank()) {
-            result = getProperties().getProperty("DATABASE_USERNAME");
+            result = ResourceUtils.getProperty("DATABASE_USERNAME");
         }
         return result;
     }
 
     protected static String getDatabasePassword() {
-        String result = getProperties().getProperty("datasource.password");
+        String result = ResourceUtils.getProperty("datasource.password");
         if(result == null || result.isBlank()) {
-            result = getProperties().getProperty("quarkus.datasource.password");
+            result = ResourceUtils.getProperty("quarkus.datasource.password");
         }
         if(result == null || result.isBlank()) {
-            result = getProperties().getProperty("DATABASE_PASSWORD");
+            result = ResourceUtils.getProperty("DATABASE_PASSWORD");
         }
         return result;
     }
 
     protected static String getDatabaseMaxDbConnectionPoolProperty() {
-        String result = getProperties().getProperty("datasource.max_db_connection_pool_size");
+        String result = ResourceUtils.getProperty("datasource.max_db_connection_pool_size");
         if(result == null || result.isBlank()) {
-            result = getProperties().getProperty("DB_CONNECTION_POOL_SIZE");
+            result = ResourceUtils.getProperty("DB_CONNECTION_POOL_SIZE");
         }
         if(result == null || result.isBlank()) {
             result = String.valueOf(DEFAULT_DB_CONNECTION_POOL_SIZE);
         }
         return result;
-    }
-
-    protected static Properties getProperties() {
-        if(properties == null || properties.isEmpty()) {
-            properties = ResourceUtils.getProperties();
-        }
-        return properties;
     }
 
     public static void setProperties(Properties properties) {
